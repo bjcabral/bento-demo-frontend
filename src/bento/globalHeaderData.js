46c137b090380d5794d9e91f780ec4fa5acc96bf
Ubuntu <ubuntu@ip-172-18-0-186.ec2.internal>
2021-03-16 23:12:37 +0000

    from ubuntu

--- src/bento/globalHeaderData.js
+++ src/bento/globalHeaderData.js
@@ -3,7 +3,7 @@ import Easter from '../assets/header/headerGraphic.png';
 // globalHeaderLogo image 468x100
 // globalHeaderImage: image 2200x100
 export default {
-  globalHeaderLogo: 'https://github.com/bjcabral/bento-demo-frontend/blob/master/src/assets/landing/SNCC-Logo.png',
+  globalHeaderLogo: 'https://raw.githubusercontent.com/bjcabral/bento-demo-frontend/master/src/assets/header/SNCC-Logo.png',
   globalHeaderLogoLink: '/',
   globalHeaderLogoAltText: 'Bento Logo',
   globalHeaderImage: Easter,
